d5b73dfa045885bffc732c25819f01bc3ef19abf
Criscoll <cristiand2021@gmail.com>
2021-01-25 13:00:31 +1100

    split page into banner and form section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from './containers/Banner';
+import Form from './containers/Form';
 import './styles/main.css';
 
 function App() {
   return (
     <React.Fragment>
       <Banner />
+      <Form />
     </React.Fragment>
   );
 }
---
+++ src/containers/Form.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/main.css';
+
+export default function Form() {
+  return (
+    <React.Fragment>
+      <div id="form-background">
+        <div className="form-container"></div>
+      </div>
+    </React.Fragment>
+  );
+}
