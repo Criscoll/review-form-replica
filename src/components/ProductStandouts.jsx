27190f82e526de826177551d8d857430100e9c26
Criscoll <cristiand2021@gmail.com>
2021-01-25 20:59:03 +1100

    buttons in progress

--- src/components/ProductStandouts.jsx
+++ src/components/ProductStandouts.jsx
@@ -5,7 +5,17 @@ import * as enumerations from '../constants/enumerations';
 export default function ProductStandouts() {
   return (
     <React.Fragment>
-      <SquareButton value={enumerations.productStandouts.accurateTimekeeping} />
+      <div className="product-standouts-wrapper">
+        <h3>Product Standouts</h3>
+        <h4>Choose up to 5 that best apply (optional)</h4>
+        <div className="product-standouts-container">
+          <SquareButton
+            value={enumerations.productStandouts.accurateTimekeeping}
+          />
+          <SquareButton value={enumerations.productStandouts.highQuality} />
+          <SquareButton value={enumerations.productStandouts.durable} />
+        </div>
+      </div>
     </React.Fragment>
   );
 }
