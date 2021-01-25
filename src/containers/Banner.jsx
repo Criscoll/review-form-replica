80f5edffcd48ddd8f7b940dff61354635ca60728
Criscoll <cristiand2021@gmail.com>
2021-01-25 12:45:23 +1100

    basic banner added

---
+++ src/containers/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/main.css';
+
+export default function Banner() {
+  return (
+    <React.Fragment>
+      <div id="banner">
+        <h1>MIADONNA</h1>
+        <h3>The Eco Diamond Specialists</h3>
+        <p>1815 Rose Gold Chronograph Watch - Brown Croco Strap</p>
+        <h2>Ratings</h2>
+      </div>
+    </React.Fragment>
+  );
+}
